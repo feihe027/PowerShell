a6edc4e1f0970783e81f6ceea5702d18475010ab
feihe027 <feihe027@163.com>
2017-05-18 01:40:40 +0800

    1. Second.

--- Excel.ps1
+++ Excel.ps1
@@ -4,3 +4,8 @@ $excel.Visible = $true
 $workbook = $excel.Workbooks.Add()
 $sheet = $workbook.Worksheets.Item(1)
 $sheet.Name = "mike"
+$lineStyle = "microsoft.office.interop.excel.xlLineStyle" -as [type]
+$colorIndex = "microsoft.office.interop.excel.xlColorIndex" -as [type]
+$borderWeight = "microsoft.office.interop.excel.xlBorderWeight" -as [type]
+$chartType = "microsoft.office.interop.excel.xlChartType" -as [type]
+
